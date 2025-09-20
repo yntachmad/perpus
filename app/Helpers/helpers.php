a8a9477fbc2f4ca401b4f87d3111df713289b580
yntachmad <achmad.t.jayanto@gmail.com>
2025-09-20 15:31:44 +0700

    factory dan seeder

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+
+if (!function_exists('flashMessage')) {
+    function flashMessage($message, $type = 'success'): void
+    {
+        session()->flash('message', $message);
+        session()->flash('type', $type);
+
+    }
+
+}
+
+if (!function_exists('userNameGenerator')) {
+    function userNameGenerator(string $name): string
+    {
+        $username = strtolower(preg_replace('/\s+/', '_', trim($name)));
+        $original_username = $username;
+        $count = 1;
+
+        while (User::where('username', $username)->exists()) {
+            $username = $original_username . $count;
+            $count++;
+        }
+
+
+        return $username;
+    }
+}
