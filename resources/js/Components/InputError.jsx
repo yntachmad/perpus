b616a33bfdbc0b5762246ad8a18ad55093da34d2
yntachmad <achmad.t.jayanto@gmail.com>
2025-09-22 09:15:44 +0700

    nav link responsive update

--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -2,7 +2,11 @@ export default function InputError({ message, className = '', ...props }) {
     return message ? (
         <p
             {...props}
-            className={'text-sm text-red-600 ' + className}
+            // className={'text-sm text-red-600 ' + className}
+            className={cn(
+                'text-xs font-medium text-red-600',
+                className
+            )}
         >
             {message}
         </p>
